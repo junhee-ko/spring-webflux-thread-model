72df6c095bd91f3126bc3c4517596cdf86d4d400
junhee-ko <junheee.ko@gmail.com>
2022-01-23 14:18:56 +0900

    first commit

---
+++ src/main/kotlin/jko/springwebfluxthreadmodel/SpringWebfluxThreadModelApplication.kt
@@ -0,0 +1,11 @@
+package jko.springwebfluxthreadmodel
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebfluxThreadModelApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringWebfluxThreadModelApplication>(*args)
+}
---
+++ src/test/kotlin/jko/springwebfluxthreadmodel/SpringWebfluxThreadModelApplicationTests.kt
@@ -0,0 +1,13 @@
+package jko.springwebfluxthreadmodel
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringWebfluxThreadModelApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
